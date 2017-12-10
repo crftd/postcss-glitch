1cd054786fa26dd20a776825929a8d49a6495f50
hex22a <hex22a@gmail.com>
2017-12-11 00:42:02 +0300

    add before and after pseudo elements

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -2,13 +2,26 @@
  * Crafted by Crash on 29.11.17.
  */
 
-import index from '../index';
+import { parse, rule } from 'postcss';
+import index, { transform } from '../index';
 
 describe('index', () => {
-  it('should throw unimplemented error', () => {
+  it('throws unimplemented error', () => {
     // Arrange
     // Act
     // Assert
     expect(index).toThrowError('Not implemented');
+  });
+
+  it('adds ::before and ::after pseudo elements to the rule that contains glitch declaration', () => {
+    // Arrange
+    const expectedCss = parse('.foo { background: black; glitch: 52px, 0, 0, 54px, 3000px, 48px, #f00, #00f, 1px; }');
+    const expectedBeforeNode = rule({ selector: '.foo::before' });
+    const expectedAfterNode = rule({ selector: '.foo::after' });
+    // Act
+    transform(expectedCss);
+    // Assert
+    expect(expectedCss.nodes[1]).toMatchObject(expectedBeforeNode);
+    expect(expectedCss.nodes[2]).toMatchObject(expectedAfterNode);
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@
  * Crafted by Crash on 29.11.17.
  */
 
+import { rule } from 'postcss';
+
+export const transform = css => {
+  css.walkDecls('glitch', declaration => {
+    const { selector } = declaration.parent;
+    declaration.parent.after(rule({ selector: `${selector}::after` }));
+    declaration.parent.after(rule({ selector: `${selector}::before` }));
+  });
+};
+
 export default () => { throw new Error('Not implemented'); };
