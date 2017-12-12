58aa3270b042e965ccc1c6d8c201612bd50f9897
hex22a <hex22a@gmail.com>
2017-12-13 01:20:53 +0300

    add before and after pseudo elements with declarations (no animation)

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -2,7 +2,7 @@
  * Crafted by Crash on 29.11.17.
  */
 
-import { parse, rule } from 'postcss';
+import { parse } from 'postcss';
 import index, { transform } from '../index';
 
 describe('index', () => {
@@ -15,13 +15,21 @@ describe('index', () => {
 
   it('adds ::before and ::after pseudo elements to the rule that contains glitch declaration', () => {
     // Arrange
-    const expectedCss = parse('.foo { background: black; glitch: 52px, 0, 0, 54px, 3000px, 48px, #f00, #00f, 1px; }');
-    const expectedBeforeNode = rule({ selector: '.foo::before' });
-    const expectedAfterNode = rule({ selector: '.foo::after' });
+    const expectedHeight = '48px';
+    const expectedSelector = '.foo';
+    const expectedShadowOffset = '1px';
+    const expectedFirstColor = '#f00';
+    const expectedSecondColor = '#00f';
+    const expectedCss = parse(`${expectedSelector} { background: black; glitch: ${expectedHeight} ${expectedFirstColor} ${expectedSecondColor} ${expectedShadowOffset}; }`);
+    const expectedResultCss = `${expectedSelector} { background: black; }
+${expectedSelector}::before, ${expectedSelector}::after { content: attr(data-text); position: absolute; top: 0; left: 0; color: #fff; background: #000; overflow: hidden; clip-path: inset(48px 0 0 0); }
+${expectedSelector}::before { text-shadow: -${expectedShadowOffset} 0 ${expectedFirstColor}; }
+${expectedSelector}::after { text-shadow: ${expectedShadowOffset} 0 ${expectedSecondColor}; }`;
+
     // Act
     transform(expectedCss);
+
     // Assert
-    expect(expectedCss.nodes[1]).toMatchObject(expectedBeforeNode);
-    expect(expectedCss.nodes[2]).toMatchObject(expectedAfterNode);
+    expect(expectedCss.toString()).toEqual(expectedResultCss);
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,13 +2,38 @@
  * Crafted by Crash on 29.11.17.
  */
 
-import { rule } from 'postcss';
+import { rule, decl, list } from 'postcss';
+
+const CONTENT_DECLARATION = decl({ prop: 'content', value: 'attr(data-text)' });
+const POSITION_DECLARATION = decl({ prop: 'position', value: 'absolute' });
+const TOP_DECLARATION = decl({ prop: 'top', value: '0' });
+const LEFT_DECLARATION = decl({ prop: 'left', value: '0' });
+const COLOR_DECLARATION = decl({ prop: 'color', value: '#fff' });
+const BACKGROUND_DECLARATION = decl({ prop: 'background', value: '#000' });
+const OVERFLOW_DECLARATION = decl({ prop: 'overflow', value: 'hidden' });
 
 export const transform = css => {
   css.walkDecls('glitch', declaration => {
+    const [height, firstColor, secondColor, shadowOffset] = list.space(declaration.value);
     const { selector } = declaration.parent;
-    declaration.parent.after(rule({ selector: `${selector}::after` }));
-    declaration.parent.after(rule({ selector: `${selector}::before` }));
+    const beforeRule = rule({ selector: `${selector}::before` });
+    beforeRule.append(decl({ prop: 'text-shadow', value: `-${shadowOffset} 0 ${firstColor}` }));
+    const afterRule = rule({ selector: `${selector}::after` });
+    afterRule.append(decl({ prop: 'text-shadow', value: `${shadowOffset} 0 ${secondColor}` }));
+    const beforeAfterRule = rule({ selectors: [`${selector}::before`, `${selector}::after`] });
+    beforeAfterRule
+      .append(CONTENT_DECLARATION)
+      .append(POSITION_DECLARATION)
+      .append(TOP_DECLARATION)
+      .append(LEFT_DECLARATION)
+      .append(COLOR_DECLARATION)
+      .append(BACKGROUND_DECLARATION)
+      .append(OVERFLOW_DECLARATION)
+      .append(decl({ prop: 'clip-path', value: `inset(${height} 0 0 0)` }));
+    declaration.parent.after(afterRule);
+    declaration.parent.after(beforeRule);
+    declaration.parent.after(beforeAfterRule);
+    declaration.remove();
   });
 };
 
