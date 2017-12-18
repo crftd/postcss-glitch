81c1ec2645b839e3ae5a6d0d1fe7caaed6b3fd1c
hex22a <hex22a@gmail.com>
2017-12-19 01:21:03 +0300

    implement clip=path builder

--- src/__tests__/clip-path.builder.test.js
+++ src/__tests__/clip-path.builder.test.js
@@ -2,13 +2,58 @@
  * Crafted by Crash on 14.12.17.
  */
 
+import { decl } from 'postcss';
 import * as builder from '../clip-path.builder';
 
 describe('clip-path builder', () => {
-  it('throws unimplemented error', () => {
+  it('returns clip-path declaration', () => {
     // Arrange
+    const expectedHeight = 51;
+    const expectedOffsetTop = 25;
+    const expectedGlitchHeight = 5;
+    const expectedOffsetBottom = 21;
+    const expectedClipPath = decl({ prop: 'clip-path', value: `inset(${expectedOffsetTop}px 0 ${expectedOffsetBottom}px 0)` });
+
+    const mockGetOffsetTop = () => expectedOffsetTop;
+    const mockGetOffsetBottom = () => expectedOffsetBottom;
+
     // Act
+    const actualClipPath = builder.default(expectedHeight, expectedGlitchHeight, mockGetOffsetTop, mockGetOffsetBottom);
+
     // Assert
-    expect(builder.default).toThrowError('Not implemented');
+    expect(actualClipPath).toEqual(expectedClipPath);
+  });
+
+  it('returns top offset', () => {
+    // Arrange
+    const expectedHeight = 51;
+    const expectedOffsetTop = 25;
+    const expectedRandomNumber = 0.5;
+
+    const mockMath = Object.create(global.Math);
+    const mockRandom = jest.fn(() => expectedRandomNumber);
+    mockMath.random = mockRandom;
+    global.Math = mockMath;
+
+    // Act
+    const actualOffsetTop = builder.getOffsetTop(expectedHeight);
+
+    // Assert
+    expect(mockRandom).toHaveBeenCalled();
+    expect(actualOffsetTop).toEqual(expectedOffsetTop);
+  });
+
+  it('returns bottom offset', () => {
+    // Arrange
+    const expectedHeight = 51;
+    const expectedOffsetTop = 25;
+    const expectedGlitchHeight = 5;
+    const expectedOffsetBottom = 21;
+
+    // Act
+    const actualOffsetBottom = builder.getOffsetBottom(expectedHeight, expectedOffsetTop, expectedGlitchHeight);
+
+    // Assert
+    expect(actualOffsetBottom).toEqual(expectedOffsetBottom);
   });
 });
--- src/clip-path.builder.js
+++ src/clip-path.builder.js
@@ -2,9 +2,13 @@
  * Crafted by Crash on 14.12.17.
  */
 
+import { decl } from 'postcss';
 
-export default () => { throw new Error('Not implemented'); };
+export const getOffsetTop = height => Math.floor(Math.random() * height);
+export const getOffsetBottom = (height, offsetTop, glitchHeight) => height - offsetTop - glitchHeight;
 
-// TODO implement
-// const getOffsetTop = () => { throw new Error('Not implemented'); };
-// const getOffsetBottom = () => { throw new Error('Not implemented'); };
+export default (height, glitchHeight, _getOffsetTop = getOffsetTop, _getOffsetBottom = getOffsetBottom) => {
+  const offsetTop = _getOffsetTop(height);
+  const offsetBottom = _getOffsetBottom(height, offsetTop, glitchHeight);
+  return decl({ prop: 'clip-path', value: `inset(${offsetTop}px 0 ${offsetBottom}px 0)` });
+};
