610452e4a38f22f42ec969c07042e89ff1ba0a11
hex22a <hex22a@gmail.com>
2017-12-19 23:19:07 +0300

    add default glitch height 5

--- src/clip-path.builder.js
+++ src/clip-path.builder.js
@@ -7,7 +7,7 @@ import { decl } from 'postcss';
 export const getOffsetTop = height => Math.floor(Math.random() * height);
 export const getOffsetBottom = (height, offsetTop, glitchHeight) => height - offsetTop - glitchHeight;
 
-export default (height, glitchHeight, _getOffsetTop = getOffsetTop, _getOffsetBottom = getOffsetBottom) => {
+export default (height, glitchHeight = 5, _getOffsetTop = getOffsetTop, _getOffsetBottom = getOffsetBottom) => {
   const offsetTop = _getOffsetTop(height);
   const offsetBottom = _getOffsetBottom(height, offsetTop, glitchHeight);
   return decl({ prop: 'clip-path', value: `inset(${offsetTop}px 0 ${offsetBottom}px 0)` });
