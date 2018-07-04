748761cfcb54ce45457c8afce084379cf8f9465f
Crash <hex22a@gmail.com>
2018-07-04 14:39:46 +0300

    Add demo

--- webpack.config.js
+++ webpack.config.js
@@ -37,23 +37,6 @@ const config = {
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
-      },
-      output: {
-        comments: false,
-      },
-    }),
   ],
 };
 
